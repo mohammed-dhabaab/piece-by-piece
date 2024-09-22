d5091200ea04be525a4abcdc063ada4539227174
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-09-22 10:23:25 +0300

    Completed the navbar

---
+++ src/pages/index/index.js
@@ -0,0 +1,6 @@
+const mobileNavbarListContainer = document.getElementById("mobile-navbar-list-container")
+
+function displayMobilNavbar() {
+    mobileNavbarListContainer.classList.toggle("none");
+}
+
